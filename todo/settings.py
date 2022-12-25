76413b974095449f3f6dfef29006b04447092de3
gunjanhyadav <gunjanhyadav96@gmail.com>
2022-12-25 16:51:47 +0530

    makechanges

--- todo/settings.py
+++ todo/settings.py
@@ -89,12 +89,15 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'todo',
+       'USER': 'postgres',
+       'PASSWORD': 'gunjanyadav',
+       'HOST': '',
+       'PORT': '5432',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -127,8 +130,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
 
 STATIC_URL = 'static/'
 
